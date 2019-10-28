8b15b0d798cf3cdc24daa230c3ec5d7d20e644b0
xXValhallaCoderXx <renate.gouveia@gmail.com>
2019-10-29 04:52:39 +0800

    Initial state for about page complete

--- src/pages/about/index-page.tsx
+++ src/pages/about/index-page.tsx
@@ -13,8 +13,8 @@ const HomePage = () => {
   return (
     <LayoutMain nav>
       <div className={styles.top_container_wrapper}>
-        <div className={styles.title_caption_wrapper}>
-          <h1 style={{fontWeight: 800}} className={`text-light ${styles.about_title}`}>
+        <div className={styles.title_wrapper}>
+          <h1 className={`text-light ${styles.title}`}>
             Not enough people in this world, I think, carry a cosmic perspective
             with them. It could be life-changing.
           </h1>
@@ -31,22 +31,22 @@ const HomePage = () => {
       </div>
       <div className={styles.bottom_container_wrapper}>
         <Container>
-          <Row style={{marginTop: 150}}>
+          <Row>
             <Col sm="12" md="6">
               <p className={styles.bottom_caption_txt}>
-                <span className="font-weight-bold">
+                <span className={styles.caption_text_2_top}>
                   THE BRAIN IS LIKE A MUSCLE,
                 </span>
                 <br />{" "}
-                <span style={{fontSize: "1.5rem"}}>
+                <span className={styles.caption_text_2_bottom}>
                   WHEN WE USE IT WE FEEL VERY GOOD, UNDERSTANDING IS JOYOUS
                 </span>
               </p>
-              <p>~Carl Sagan</p>
+              <p className={styles.caption_text_2_author}>~Carl Sagan</p>
             </Col>
             <Col sm="12" md="6">
-              <div className={styles.text_border}>
-                <p style={{fontSize: 22}}>
+              <div className={styles.intro_text_wrapper}>
+                <p className={styles.intro_text}>
                   I have always had a keen interest in space, though it wasnt
                   over the recent few years my passion for it increasingly grew.
                   <br />
@@ -58,14 +58,12 @@ const HomePage = () => {
             </Col>
           </Row>
           <Row style={{marginTop: 150}}>
-            <Col md="6">
+            <Col md="6" className={styles.icon_wrapper_left}>
               <img src={magnifyIcon} height="200" />
             </Col>
             <Col md="6">
-              <p style={{fontSize: 30, fontWeight: 800, textAlign: "right"}}>
-                I EXPLORED
-              </p>
-              <p style={{fontSize: 22, textAlign: "right"}}>
+              <p className={styles.icon_text_right_title}>I EXPLORED</p>
+              <p className={styles.icon_text_right}>
                 I was searching around for Space infomational websites for the
                 younger generation, and found most of them to be quite bland or
                 difficult to navigate. This is what set the notion to create
@@ -74,44 +72,40 @@ const HomePage = () => {
             </Col>
           </Row>
           <Row style={{marginTop: 150, marginBottom: 150}}>
-            <Col md="6">
-              <p style={{fontSize: 30, fontWeight: 800, textAlign: "left"}}>
-                THE VISION
-              </p>
-              <p style={{fontSize: 22, textAlign: "left"}}>
+            <Col md="6" className="order-2 order-sm-1">
+              <p className={styles.icon_text_left_title}>THE VISION</p>
+              <p className={styles.icon_text_left}>
                 To be an infomational Space website for a younger audience, with
                 hopes of increasing awareness, and spark curiosity in others,
                 inspiring them to start to look upon the stars.
               </p>
             </Col>
-            <Col className="d-flex justify-content-end align-items-center" md="6">
+            <Col
+              className={`${styles.icon_wrapper_right} order-1 order-sm-2`}
+              md="6">
               <img src={visionIcon} height="160" />
             </Col>
           </Row>
           <Row style={{marginTop: 150, marginBottom: 150}}>
-            <Col md="6">
+            <Col md="6" className={styles.icon_wrapper_left}>
               <img src={missionIcon} height="200" />
             </Col>
             <Col md="6">
-              <p style={{fontSize: 30, fontWeight: 800, textAlign: "right"}}>
-                THE MISSION
-              </p>
-              <p style={{fontSize: 22, textAlign: "right"}}>
+              <p className={styles.icon_text_right_title}>THE MISSION</p>
+              <p className={styles.icon_text_right}>
                 If this site sparks the curiosity of just one person to look
                 towards the stars, then this website has fufilled its duty.
               </p>
             </Col>
           </Row>
         </Container>
-        <div
-          className="d-flex justify-content-center align-items-center flex-column"
-          style={{backgroundColor: "black", height: 200}}>
-          <p className="text-light mb-n1" style={{fontSize: "2rem"}}>
+        <div className="bg-dark p-5">
+          <h2 className={`${styles.footer_text} text-light`}>
             Excited, and curious?
-          </p>
-          <p className="text-light" style={{fontSize: "2rem"}}>
+          </h2>
+          <h2 className={`${styles.footer_text} text-light mb-n1`}>
             You can only do one thing, uncover the mysteries of the cosmos!
-          </p>
+          </h2>
         </div>
       </div>
     </LayoutMain>
