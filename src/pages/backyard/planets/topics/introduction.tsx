77ba77d5af4d94d1ebb35b309b0cdfeb2a459a87
Renate <renate.gouveia@gmail.com>
2019-11-26 03:28:37 +0800

    Modify planets page

--- src/pages/backyard/planets/topics/introduction.tsx
+++ src/pages/backyard/planets/topics/introduction.tsx
@@ -14,12 +14,14 @@ const PlanetsIntro = () => {
         <Col sm="12" className="p-4">
           <h1 className={`${styles.title} mt-5`}>Introduction</h1>
           <p className={`${styles.main_text} mt-3 p-0`}>
-            Our friendly <i>(well...some not so)</i> neighbours!
+            Our <i>(some not so)</i> friendly neighbours!
           </p>
           <p className={`${styles.main_text} mt-3 p-0`}>
-            There are <strong>8</strong> (not 9?) planets in our solar system,
-            they are Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and
-            Neptune.
+            There are <strong>8</strong> <i>(not 9?)</i> planets in our solar
+            system, they are{" "}
+            <i>
+              Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and Neptune.
+            </i>
           </p>
         </Col>
       </Row>
@@ -30,21 +32,25 @@ const PlanetsIntro = () => {
         <Col sm="12" md="7" className={`${styles.why_8_wrapper} p-4`}>
           <h3 className={styles.sub_title}>Why only 8 planets?</h3>
           <p className={`${styles.main_text} mt-3 p-0`}>
-            This can be a touchy subject and lead to heated debates, Officiall,
-            there are 8 planets, the IAU (International Astronomical Union)
-            removed Pluto from the list of what had been nine planets in the
-            Solar System in August 2006, due to not meeting the{" "}
-            <a href="https://www.iau.org/static/resolutions/Resolution_GA26-5-6.pdf">
+            This can be a touchy subject and lead to heated debates, Offically,
+            there are 8 planets. The <span style={{fontWeight: 800}}>IAU</span>{" "}
+            <i>(International Astronomical Union)</i> removed Pluto from the
+            list of what had been nine planets in the Solar System in August
+            2006, due to not meeting the{" "}
+            <a
+              target="_blank"
+              href="https://www.iau.org/static/resolutions/Resolution_GA26-5-6.pdf">
               requirements
             </a>
             .
           </p>
           <p className={`${styles.main_text} mt-3 p-0`}>
-            Though there are many people, even from NASA itself to this{" "}
-            <a href="https://www.sciencealert.com/nasa-administrator-jim-bridenstine-says-pluto-is-a-planet">
-              day
+            Though many will disagree with this decision,{" "}
+            <a
+              target="_blank"
+              href="https://www.sciencealert.com/nasa-administrator-jim-bridenstine-says-pluto-is-a-planet">
+              some even from within NASA.
             </a>
-            , would disagree with the descison of the IAU.
           </p>
         </Col>
       </Row>
@@ -69,20 +75,20 @@ const PlanetsIntro = () => {
         of planet, as voted on by the IAU in 2006, is as follows:
       </p>
       <div className="rounded overflow-hidden shadow-sm mt-5 bg-dark text-light p-4">
-        <p className={`${styles.sub_title} p-0`}>
+        <h3 className={`${styles.sub_title} p-0 pl-3`}>
           A planet is a celestial body that
-        </p>
+        </h3>
         <div className="p-3">
           <p className={`${styles.sub_text} p-0`}>
-            A - Is in orbit around the Sun
+            1 - Is in orbit around the Sun
           </p>
           <p className={`${styles.sub_text} p-0`}>
-            B - Has sufficient mass for its self-gravity to overcome rigid body
+            2 - Has sufficient mass for its self-gravity to overcome rigid body
             forces so that it assumes a hydrostatic equilibrium (nearly round)
             shape
           </p>
           <p className={`${styles.sub_text} p-0`}>
-            C - Has cleared the neighbourhood around its orbit.
+            3 - Has cleared the neighbourhood around its orbit.
           </p>
         </div>
       </div>
