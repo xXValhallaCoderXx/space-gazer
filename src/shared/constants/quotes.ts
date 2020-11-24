2356f9d767ad2483ed4ae420898edfeeccaaa9f6
Renate Gouveia <renate.gouveia@gmail.com>
2020-11-25 03:40:53 +0800

    Update solar system module

---
+++ src/shared/constants/quotes.ts
@@ -0,0 +1,32 @@
+export const QUOTES = {
+    robertSherwood: {
+        quote: "We all come from our own little planets. That's why we're all different. That's what makes life interesting.",
+        author: "Robert E. Sherwood"
+    },
+    clementStone: {
+        author: "W. Clement Stone",
+        quote: "Aim for the moon, if you miss, you may hit a star"
+    }
+}
+
+
+
+export const PLANET_DROPDOWN_OPTIONS = [
+    { value: "intro", label: "Introduction" },
+    { value: "mercury", label: "Mercury", disabled: "yes" },
+    { value: "venus", label: "Venus", disabled: "yes" },
+    { value: "earth", label: "Earth", disabled: "yes" },
+    { value: "mars", label: "Mars", disabled: "yes" },
+    { value: "jupiter", label: "Jupiter", disabled: "yes" },
+    { value: "saturn", label: "Saturn", disabled: "yes" },
+    { value: "uranus", label: "Uranus", disabled: "yes" },
+    { value: "neptune", label: "Neptune", disabled: "yes" }
+];
+
+export const SOLAR_SYSTEM_OPTIONS = [
+    { value: "intro", label: "Introduction" },
+    { value: "solar-system", label: "Solar System" },
+    { value: "eclipse", label: "Solar Eclipse", disabled: "yes" },
+    { value: "comets", label: "Comets", disabled: "yes" },
+    { value: "asteroids", label: "Asteroids", disabled: "yes" }
+  ];
