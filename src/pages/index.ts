7e4025b8a40f4a75fdb325217de374301f8df95b
Renate Gouveia <renate.gouveia@gmail.com>
2020-11-25 02:03:19 +0800

    Refactor backyard pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
-export {SolarSystem, Moons, Planets} from "./backyard";
 export {About} from "./about";
 export {default as NotFound} from "./not-found";
-export {default as Loading} from "./loading";
+export {default as SplashScreen} from "./splash-screen";
 export {default as Home} from "./home";
 export {default as Alone} from "./alone";
---
+++ src/pages/solar-system/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import { ParallaxProvider } from "react-scroll-parallax";
+
+import { Route, Switch } from "react-router-dom";
+
+import SolarSystemModule from "./solar-system"
+import PlanetsModule from "./planets"
+import MoonsModule from "./moons"
+
+
+const SolarSystemRoutes = () => {
+    return (
+        <Switch>
+            <Route exact path="/solar-system/main" component={SolarSystemModule} />
+            <Route exact path="/solar-system/main/:tab" component={SolarSystemModule} />
+            <Route exact path="/solar-system/planets" component={PlanetsModule} />
+            <Route exact path="/solar-system/planets/:tab" component={PlanetsModule} />
+            <Route exact path="/solar-system/moons" component={MoonsModule} />
+            <Route exact path="/solar-system/moons/:tab" component={MoonsModule} />
+        </Switch>
+    );
+};
+
+export default SolarSystemRoutes;
